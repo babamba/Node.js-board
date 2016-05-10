3d45d9405ff578e2a7d31271609978149507a466
rlawlsdnjs <orochi13@naver.com>
2016-05-10 16:40:21 +0900

    board

---
+++ app.js
@@ -0,0 +1,45 @@
+//수정
+var express  = require('express');
+var app      = express();
+var path     = require('path');
+var mongoose = require('mongoose');
+var session  = require('express-session');
+var flash    = require('connect-flash');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+// database
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once("open",function () {
+  console.log("DB connected!");
+});
+db.on("error",function (err) {
+  console.log("DB ERROR :", err);
+});
+
+// view engine
+app.set("view engine", 'ejs');
+
+// middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(flash());
+app.use(session({secret:'MySecret'}));
+
+// passport
+var passport = require('./config/passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
+app.use('/', require('./routes/home'));
+app.use('/users', require('./routes/users'));
+app.use('/posts', require('./routes/posts'));
+
+// start server
+app.listen(3003, function(){
+  console.log('Server On!');
+});
